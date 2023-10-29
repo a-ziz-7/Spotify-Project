492158e10bdf76826d4a2058ae3e59f95dfd92db
a-ziz-7 <129803663+a-ziz-7@users.noreply.github.com>
2023-10-29 17:27:00 -0400

    Update spotify.py

--- spotify.py
+++ spotify.py
@@ -2,28 +2,51 @@ import requests
 # get private data from credentials file
 from credentials import token, client_id, lms, analysis
 
-# main file
+# Create a new playlist
 def create_playlist(name, public):
-    response = requests.post(
-        lms,
-        headers={
-            "Authorization": f"Bearer {token}"
-        },
-        json={
-            "name": name,
-            "public": public
-        }
-    )
-    json_resp = response.json()
-    return json_resp
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+    data = {
+        "name": name,
+        "public": public
+    }
+    response = requests.post(lms, headers=headers, json=data)
+
+    if response.status_code == 200:
+        json_resp = response.json()
+        return json_resp
+    else:
+        print(f"Failed to create a playlist. Status code: {response.status_code}")
+        return None
 
 
+# Make an analysis request
 def a():
-    r = requests.post(analysis, f"Bearer {token}")
-    r_r = r.json()
-    return r_r
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+    r = requests.post(analysis, headers=headers)
+
+    if r.status_code == 200:
+        r_r = r.json()
+        return r_r
+    else:
+        print(f"Failed to make an analysis request. Status code: {r.status_code}")
+        return None
 
 
 if __name__ == '__main__':
-    r = a()
-    print(r)
+    # Create a new playlist
+    playlist_name = "My Playlist"
+    is_public = True  # Set to True if you want the playlist to be public
+    playlist_result = create_playlist(playlist_name, is_public)
+
+    if playlist_result:
+        print(f"Playlist created: {playlist_result}")
+
+    # Make an analysis request
+    analysis_result = a()
+
+    if analysis_result:
+        print(f"Analysis result: {analysis_result}")
